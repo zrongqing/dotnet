7e79cc89cd3f3e3f2eebc7bb558025d3e0edd408
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-05-08 17:51:48 +0800

    11

--- ZRQ.Test/Program.cs
+++ ZRQ.Test/Program.cs
@@ -1,25 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data.SqlTypes;
 using System.Text.RegularExpressions;
+using ZRQ.Test;
 
-
-string reportNumber = "001";
-
-var matches = Regex.Matches(reportNumber, "[0-9]+");
-var numbers = new List<string>();
-foreach (Match match in matches)
-{
-    numbers.Add(match.Value);
-}
-
-int lastIndex = reportNumber.LastIndexOf(numbers[numbers.Count - 1]);
-if (lastIndex == 0)
-{
-}
-else
-{
-    String Prefix = reportNumber.Substring(0, lastIndex);
-}
 
 
 
