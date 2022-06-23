63a48b9d5a885e5923efb2e87ed2d496fcee8797
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-06-23 09:38:02 +0800

    Delegate测试

---
+++ Example/ZRQ.Example.Delegate/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZRQ.Example.Delegate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Update = new Update();
+        }
+
+        private Update Update { get; set; }
+
+        private void Btn_Init_Click(object sender, RoutedEventArgs e)
+        {
+            this.Update = new Update();
+            Update.UpdateComparator = AAAA;
+        }
+
+        public int AAAA(int left, int right)
+        {
+            return left + right;
+        }
+
+
+        private void Btn_Update_Click(object sender, RoutedEventArgs e)
+        {
+            this.Update.Excute();
+        }
+    }
+}
---
+++ Example/ZRQ.Example.Delegate/Update.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace ZRQ.Example.Delegate
+{
+
+    public class Update
+    {
+        public delegate int Comparison<in T>(T left, T right);
+
+        // Declare an instance of that type:
+        public static Comparison<int> UpdateComparator;
+
+        public void Excute()
+        {
+            UpdateComparator(1, 1);
+        }
+    }
+}
