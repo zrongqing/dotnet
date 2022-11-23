bf98ccd8c3b9555dd0956327d80b5ff18de280dd
zrq-github <46354863+zrq-github@users.noreply.github.com>
2022-11-23 20:56:35 +0800

    LineCounter

---
+++ ZRQ.Utility/LineCounter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.Util;
+
+public static class LineCounter
+{
+    #region How to using
+    // Use the first argument as the directory
+    // to search, or default to the current directory
+    private static void HowUsing(string[] args)
+    {
+        int totalLineCount = 0;
+        string directory;
+        if (args.Length > 0)
+        {
+            directory = args[0];
+        }
+        else
+        {
+            directory = Directory.GetCurrentDirectory();
+        }
+        totalLineCount = DirectoryCountLines(directory);
+        System.Console.WriteLine(totalLineCount);
+    }
+    #endregion
+
+    public static int DirectoryCountLines(string directory)
+    {
+        int lineCount = 0;
+        foreach (string file in
+            Directory.GetFiles(directory, "*.cs"))
+        {
+            lineCount += CountLines(file);
+        }
+
+        foreach (string subdirectory in
+            Directory.GetDirectories(directory))
+        {
+            lineCount += DirectoryCountLines(subdirectory);
+        }
+
+        return lineCount;
+    }
+
+    private static int CountLines(string file)
+    {
+        string? line;
+        int lineCount = 0;
+        FileStream stream =
+            new FileStream(file, FileMode.Open);
+        StreamReader reader = new StreamReader(stream);
+        line = reader.ReadLine();
+
+        while (line != null)
+        {
+            if (line.Trim() != "")
+            {
+                lineCount++;
+            }
+            line = reader.ReadLine();
+        }
+
+        reader.Dispose();  // Automatically closes the stream
+        return lineCount;
+    }
+}
