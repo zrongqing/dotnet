8b2ebcc42c218689a655dc2dae9b967d7b947428
zrq-github <zrq.gogle@gmail.com>
2023-02-09 20:38:56 +0800

    add RelfectUtils

---
+++ ZRQ.Utility/RelfectUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.Utils
+{
+    public static class RelfectUtils
+    {
+        public static List<Attribute> GetAttributes(object ob)
+        {
+            PropertyInfo[] properties =
+                ob.GetType().GetProperties();
+
+            List<Attribute> attributes = new();
+            foreach (PropertyInfo property in properties)
+            {
+                Attribute[] propertyAttributes = (Attribute[])property.GetCustomAttributes(true);
+                attributes.AddRange(propertyAttributes);
+            }
+
+            return attributes;
+        }
+    }
+}
