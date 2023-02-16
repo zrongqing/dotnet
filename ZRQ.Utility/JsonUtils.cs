89d1b23c1c4fbe8b21986ca7b0309b6385d6b46c
zrq-github <zrq.gogle@gmail.com>
2023-02-16 19:48:07 +0800

    jsonUtils

---
+++ ZRQ.Utility/JsonUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ZRQ.Utils.Config;
+
+namespace ZRQ.Utils
+{
+    public static class JsonUtils
+    {
+        public static readonly JsonSerializerOptions Options = GetJsonSerializerOptions();
+
+        private static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            JsonSerializerOptions options = new()
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                WriteIndented = true, // 对齐写入
+                PropertyNameCaseInsensitive = true // 不区分大小写的属性名称
+            };
+            return options;
+        }
+
+        public static void SaveJsonFile(string filePath, object jsonObject)
+        {
+            if (!File.Exists(filePath))
+            {
+                // 创建一个config
+                FileUtils.CreateFile(filePath);
+            }
+
+            var jsonString = JsonSerializer.Serialize(jsonObject, Options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
