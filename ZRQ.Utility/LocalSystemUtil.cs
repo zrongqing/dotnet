6e4538fb294323dafdc3ad32d1abdd6777f88967
zrq <wyzrq163@163.com>
2024-03-23 11:25:53 +0800

    读取系统名字

---
+++ ZRQ.Utility/LocalSystemUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRQ.Utils
+{
+    public static class LocalSystemUtil
+    {
+        public static string GetComputerName()
+        {
+            return Environment.MachineName;
+        }
+    }
+}
