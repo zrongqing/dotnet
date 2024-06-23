51f7d85e2ebd59c2cd6066924359f1599c659d47
zrq <wyzrq163@163.com>
2024-06-23 21:24:40 +0800

    [EFCore] 增删查改

--- srcLearn/EFCore/EFCoreBasics/EFCoreBasics/Program.cs
+++ srcLearn/EFCore/EFCoreBasics/EFCoreBasics/Program.cs
@@ -7,60 +7,54 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //using (var context = new BloggingContext())
-        //{
-        //    // 确保数据库已创建
-        //    context.Database.EnsureCreated();
-        //    var canConnect = context.Database.CanConnect();
+        // 增加
+        using (var context = new ContosoPizzaContext())
+        {
+            var veggieSpecial = new Product
+            {
+                Name = "Veggie Special Pizza",
+                Price = 9.99M,
+                Description = string.Empty
+            };
+            context.Products.Add(veggieSpecial);
 
-        //    // 创建新博客
-        //    var blog = new Blog { Url = "http://sample.com" };
-        //    context.Blogs.Add(blog);
-        //    context.SaveChanges();
+            var deluxeMeat = new Product
+            {
+                Name = "Deluxe Meat Pizza",
+                Price = 12.99M,
+                Description = string.Empty
+            };
+            context.Add(deluxeMeat);
 
-        //    // 读取博客
-        //    var blogs = context.Blogs.ToList();
-        //    Console.WriteLine("All blogs in the database:");
-        //    foreach (var b in blogs)
-        //    {
-        //        Console.WriteLine($" - {b.Url}");
-        //    }
+            // context.Products.Add(veggieSpecial);
+            // context.Add(deluxeMeat);
+            // 这两个是一致的，EFCore会自动推断
 
-        //    // 更新博客
-        //    var firstBlog = context.Blogs.First();
-        //    firstBlog.Url = "http://updatedsample.com";
-        //    context.SaveChanges();
+            context.SaveChanges();
+        }
+        // 查询
+        using (var context = new ContosoPizzaContext())
+        {
+            var products = context.Products
+                .Where(p => p.Price > 10.00M)
+                .OrderBy(p => p.Name).ToList();
 
-        //    // 删除博客
-        //    context.Blogs.Remove(firstBlog);
-        //    context.SaveChanges();
-        //}
-
-
-        //using (var context = new AppDbContext())
-        //{
-        //    var isCreate = context.Database.CanConnect();
-
-        //    var employee = new Employee
-        //    {
-        //        EmployeeId = 1,
-        //        FirstName = "Sahana",
-        //        LastName = "Bhat",
-        //        Salary = 50000
-        //    };
-
-        //    context.Employees.Add(employee);
-        //    context.SaveChanges();
-
-        //    context.Employees.Remove(employee);
-        //    context.SaveChanges(true);
-
-        //    context.Employees.Update(employee);
-        //    context.SaveChanges();
-        //}
-
-        //;
-
-        //Console.WriteLine("Hello, World!");
+            foreach (var p in products)
+            {
+                Console.WriteLine($"Id:    {p.Id}");
+                Console.WriteLine($"Name:  {p.Name}");
+                Console.WriteLine($"Price: {p.Price}");
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+        // 删除
+        using (var context = new ContosoPizzaContext())
+        {
+            var delP = context.Products.FirstOrDefault(p => p.Id == 5);
+            if(delP != null)
+            {
+                context.Products.Remove(delP);
+            }
+        }
     }
 }
