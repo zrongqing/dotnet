4aaf965f0581c8e0af7f09298e1c1043fc66b99d
zrq <wyzrq163@163.com>
2025-02-04 22:02:53 +0800

    ListView

---
+++ src/UiDesktopApp/Models/TreeNode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+
+namespace UiDesktopApp.Models;
+
+public partial class TreeNode:ObservableObject
+{
+    [ObservableProperty]
+    private string _name = string.Empty;
+
+    [ObservableProperty]
+    private ObservableCollection<TreeNode> _children = [];
+}
--- src/UiDesktopApp/ViewModels/Pages/ListViewModel.cs
+++ src/UiDesktopApp/ViewModels/Pages/ListViewModel.cs
@@ -1,11 +1,28 @@
-﻿using App.UI;
+﻿using System.Collections.ObjectModel;
+using App.UI;
+using UiDesktopApp.Models;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace UiDesktopApp.ViewModels.Pages;
 
-[ObservableObject]
-public partial class ListViewModel :INavigationAware,IAppViewModel
+public partial class ListViewModel : ObservableObject, INavigationAware,IAppViewModel
 {
+    [ObservableProperty]
+    private ObservableCollection<TreeNode> _treeNodes;
+
+    public ListViewModel()
+    {
+        // Initialize with some sample data
+        TreeNodes = new ObservableCollection<TreeNode>
+        {
+            new TreeNode { Name = "Node 1" },
+            new TreeNode { Name = "Node 2", Children = new ObservableCollection<TreeNode>
+            {
+                new TreeNode { Name = "Child Node 2.1" }
+            }}
+        };
+    }
+    
     public Task OnNavigatedToAsync()
     {
         return Task.CompletedTask;
